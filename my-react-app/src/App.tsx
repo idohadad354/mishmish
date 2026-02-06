9033af153e78e8af1c2710c98facade1e201bd8e
poundfhish <myty5074@gmail.com>
2026-02-06 22:21:20 +0200

    add comment

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// we are all gonna hitler make it brah
+
 function App() {
   const [count, setCount] = useState(0)
 
